37a13bb557fd5416d0c1dd742c0b0cb489123967
Patrick Maia <patrickjourdanmaia@gmail.com>
2014-08-20 13:02:56 -0300

    uses puppet to do initial setup

---
+++ pixelated/manifests/pixelated-dev.pp
@@ -0,0 +1,46 @@
+package { 
+	"git":
+	  ensure   => installed,
+	  provider => apt;
+
+	"libssl-dev":
+	  ensure => installed,
+	  provider => apt;
+	
+	"python-setuptools":
+	  ensure => installed,
+	  provider => apt;
+	
+	"python-dev":
+	  ensure => installed,
+	  provider => apt;
+	
+	"libffi-dev":
+	  ensure => installed,
+	  provider => apt;
+	
+	"libsqlite3-dev":
+	  ensure => installed,
+	  provider => apt;
+	
+	"nodejs":
+	  ensure => installed,
+	  provider => apt;
+	
+	"nodejs-legacy":
+	  ensure => installed,
+	  provider => apt;
+	
+	"npm":
+	  ensure => installed,
+	  provider => apt;
+	
+	"bundler":
+	  ensure   => installed,
+	  provider => gem;
+}
+
+#https://github.com/sstephenson/rbenv#installation
+#https://github.com/sstephenson/ruby-build#readme
+
+#sudo easy_install virtualenv
